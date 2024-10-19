6ab3567c253d5c07ffb0997a1de6b973df2a2c25
christiandiaz24 <146766761+christiandiaz24@users.noreply.github.com>
2024-10-19 17:26:15 -0400

    Create UserTest.java

---
+++ app/src/test/java/com/example/myapplication/UserTest.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    // declaring two  instances of the user class for testing
+    private User user1;
+    private User user2;
+
+    // @Before ensures this method is run before test cases
+    // It sets up the initial state by creating instances of User class
+    @Before
+    public void setUp() {
+        user1 = new User("John Doe", "password123");
+        user2 = new User("Jane Doe", "password456");
+    }
+
+    @Test
+    public void testSetUserName() {
+        user1.setUserName("John Doe");
+        user2.setUserName("Jane Doe");
+    }
+    @Test
+    public void testGetUserName() {
+        assertEquals("John Doe", user1.getUserName());
+        assertEquals("Jane Doe", user2.getUserName());
+    }
+
+    @Test
+    public void testSetPassword() {
+        user1.setPassword("password123");
+        user2.setPassword("password456");
+    }
+
+    @Test
+    public void testGetPassword() {
+        assertEquals("password123", user1.getPassword());
+        assertEquals("password456", user2.getPassword());
+    }
+}
